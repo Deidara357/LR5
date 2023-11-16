4a24322dd969de5f80121e72b9d07cb9f4c72f6e
Deidara357 <deidara357357@gmail.com>
2023-11-16 21:02:36 +0200

    Everything Completed

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function validateForm() {
     var pib = document.getElementById('pib').value;
-    var variant = document.getElementById('variant').value;
+    var group = document.getElementById('group').value;
     var phone = document.getElementById('phone').value;
+    var idCard = document.getElementById('idCard').value;
     var faculty = document.getElementById('faculty').value;
-    var address = document.getElementById('address').value;
   
-    // Регулярні вирази для перевірки валідності даних
     var pibRegex = /^[А-ЯІЇЄ][а-яіїє]+\s[А-ЯІЇЄ]\.[А-ЯІЇЄ]\.$/;
-    var variantRegex = /^\d{1,2}$/;
+    var groupRegex = /^[А-ЯІЇЄ]{2}-\d{2}$/;
     var phoneRegex = /^\(\d{3}\)-\d{3}-\d{2}-\d{2}$/;
-    var facultyRegex = /^[А-ЯІЇЄ][а-яіїє]+$/;
-    var addressRegex = /^м\.\s[А-ЯІЇЄ][а-яіїє]+$/;
+    var idCardRegex = /^[А-ЯІЇЄ]{2} №\d{6}$/;
+    var facultyRegex = /^[А-ЯІЇЄ]{4}$/;
   
     var isValid = true;
   
@@ -23,11 +22,11 @@ function validateForm() {
     }
   
     // Перевірка варіанту
-    if (!variantRegex.test(variant)) {
-      document.getElementById('variant').classList.add('error');
+    if (!groupRegex.test(group)) {
+      document.getElementById('group').classList.add('error');
       isValid = false;
     } else {
-      document.getElementById('variant').classList.remove('error');
+      document.getElementById('group').classList.remove('error');
     }
   
     // Перевірка телефону
@@ -38,6 +37,14 @@ function validateForm() {
       document.getElementById('phone').classList.remove('error');
     }
   
+    // Перевірка idCard
+    if (!idCardRegex.test(idCard)) {
+      document.getElementById('idCard').classList.add('error');
+      isValid = false;
+    } else {
+      document.getElementById('idCard').classList.remove('error');
+    }
+
     // Перевірка факультету
     if (!facultyRegex.test(faculty)) {
       document.getElementById('faculty').classList.add('error');
@@ -45,104 +52,102 @@ function validateForm() {
     } else {
       document.getElementById('faculty').classList.remove('error');
     }
-  
-    // Перевірка адреси
-    if (!addressRegex.test(address)) {
-      document.getElementById('address').classList.add('error');
-      isValid = false;
-    } else {
-      document.getElementById('address').classList.remove('error');
-    }
-  
+
     // Виведення результатів
     if (isValid) {
-      alert('Всі дані введені правильно:\nПІБ: ' + pib + '\nВаріант: ' + variant + '\nТелефон: ' + phone + '\nФакультет: ' + faculty + '\nАдреса: ' + address);
+      alert('Всі дані введені правильно:\nПІБ: ' + pib + '\nВаріант: ' + group + '\nТелефон: ' + phone + '\nФакультет: ' + idCard + '\nАдреса: ' + faculty);
     } else {
       alert('Будь ласка, перевірте введені дані.');
     }
 }
-  
+
 
 // Функція для генерації випадкового кольору
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  }
+  return color;
 }
-  
-  // Функція для інверсії кольору з формату RGB
+
+// Функція для інверсії кольору з формату RGB
 function invertColor(color) {
-    // Розділяємо значення RGB
-    var rgbValues = color.match(/\d+/g);
-  
-    // Інвертуємо кожен канал кольору та віднімаємо від 255
-    var invertedRed = 255 - parseInt(rgbValues[0]);
-    var invertedGreen = 255 - parseInt(rgbValues[1]);
-    var invertedBlue = 255 - parseInt(rgbValues[2]);
-  
-    // З'єднуємо інвертовані канали та повертаємо результат
-    return 'rgb(' + invertedRed + ',' + invertedGreen + ',' + invertedBlue + ')';
+  var rgbValues = color.match(/\d+/g);
+  var invertedRed = 255 - parseInt(rgbValues[0]);
+  var invertedGreen = 255 - parseInt(rgbValues[1]);
+  var invertedBlue = 255 - parseInt(rgbValues[2]);
+  return 'rgb(' + invertedRed + ',' + invertedGreen + ',' + invertedBlue + ')';
 }
 
-// Функція для зміни кольору при наведенні на клітинку "22"
-function changeColorOnHover22(element) {
-    element.addEventListener('mouseover', function () {
-      if (element.innerText == '22') {
-        element.style.backgroundColor = getRandomColor();
+// Функція для зміни кольору при наведенні на клітинку з номером 17
+function changeColorOnHover17(element) {
+  element.addEventListener('mouseover', function () {
+      if (element.innerText == '17') {
+          element.style.backgroundColor = getRandomColor();
       }
-    });
-}
-  
-// Функція для зміни кольору при кліку на клітинку "22"
-function changeColorOnClick22(element) {
-    element.addEventListener('click', function () {
-      if (element.innerText === '22') {
-        element.classList.toggle('selected');
-        element.style.backgroundColor = invertColor(element.style.backgroundColor);
-      }
-    });
+  });
 }
 
-// Функція для зміни кольору стовпця при подвійному кліку
-function changeColumnColorOnDoubleClick(table, columnIndex) {
-    table.addEventListener('dblclick', function () {
-      var rows = table.rows;
-      var hasCell22 = false;
-  
-      // Перевірка, чи є в поточному стовпці клітинка зі значенням "22"
-      for (var i = 0; i < rows.length; i++) {
-        if (rows[i].cells[columnIndex].innerText === '22' && rows[i].cells[columnIndex].classList.contains('selected')) {
-          hasCell22 = true;
-          break;
-        }
-      }
-  
-      // Якщо є клітинка "22" в стовпці та вона відзначена як вибрана, то змінюємо кольори
-      if (hasCell22) {
-        var color = getRandomColor();
-        for (var i = 0; i < rows.length; i++) {
-          rows[i].cells[columnIndex].style.backgroundColor = color;
-        }
-      }
-    });
+// Функція для відображення палітри при кліку на клітинку з номером 17
+function showColorPalette(element) {
+  var colorPalette = document.getElementById('colorPalette');
+  var colorPicker = document.getElementById('colorPicker');
+  var okButton = document.getElementById('okButton');
+
+  colorPalette.style.top = element.offsetTop + 'px';
+  colorPalette.style.left = element.offsetLeft + 'px';
+  colorPalette.style.display = 'block';
+
+  okButton.addEventListener('click', function () {
+      element.style.backgroundColor = colorPicker.value;
+      colorPalette.style.display = 'none';
+  });
 }
-  
-  
-// Створення таблиці та налаштування обробників подій
+
+// Функція для зміни кольору рядків
+function changeRowColor(rowNum, color) {
+  var rows = table.getElementsByTagName('tr');
+  rows[rowNum].style.backgroundColor = color;
+}
+
+// Функція для обробки події DoubleClick
+function handleDoubleClick(cell) {
+  var cellValue = parseInt(cell.innerText);
+
+  // Перевірка чи натискання було на клітинці з номером "17"
+  if (cellValue === 17) {
+      var rowIndex = cell.parentNode.rowIndex;
+
+      // Зміна кольору рядків, починаючи з обраного і через один
+      changeRowColor(rowIndex, 'lightblue');
+      changeRowColor(rowIndex + 2, 'lightblue');
+  }
+}
+
+// Створення таблиці
 var table = document.getElementById('myTable');
+
+//Налаштування викликів функцій
 for (var i = 0; i < 6; i++) {
-    var row = table.insertRow();
-    for (var j = 0; j < 6; j++) {
-        var cell = row.insertCell();
-        var cellValue = i * 6 + j + 1;
-        cell.innerText = cellValue;
-  
-        // Виклик нової функції для клітинок зі значенням "22"
-        changeColorOnHover22(cell);
-        changeColorOnClick22(cell);
-        changeColumnColorOnDoubleClick(table, j);
-    }
+  var row = table.insertRow();
+  for (var j = 0; j < 6; j++) {
+      var cell = row.insertCell();
+      var cellValue = i * 6 + j + 1;
+      cell.innerText = cellValue;
+
+      //Виклик функцій
+      changeColorOnHover17(cell);
+      
+      cell.addEventListener('click', function () {
+          if (this.innerText === '17') {
+              showColorPalette(this);
+          }
+      });
+
+      cell.addEventListener('dblclick', function () {
+        handleDoubleClick(this);
+      });
+  }
 }
